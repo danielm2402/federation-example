4f5f68ae30182a16eb1743e24b36f7faefaaaa5f
danielm2402 <dani.m16@hotmail.com>
2024-04-18 14:53:56 -0500

    added basic example

---
+++ host2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+
+	return (
+		<div style={{
+			margin: "10px",
+			padding: "10px",
+			textAlign: "center",
+			backgroundColor: "cyan"
+		}}>
+			<h1 >App 2</h1>
+		</div>
+	);
+};
